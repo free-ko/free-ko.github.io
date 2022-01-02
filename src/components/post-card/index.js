9aca58fe1db12a419be3f8ce4085ba9eef86d9fc
kyw <47565280+free-ko@users.noreply.github.com>
2022-01-02 22:22:35 +0900

    [FE] 블로그 스타일 변경

--- src/components/post-card/index.js
+++ src/components/post-card/index.js
@@ -8,17 +8,17 @@ const PostCard = ({ post }) => {
   return (
     <div className="post-card-wrapper">
       <Link className="post-card" key={id} to={slug}>
+        <div className="categories">
+          {categories.map((category) => (
+            <span className="category" key={category} to={`/posts/${category}`}>
+              {category}
+            </span>
+          ))}
+        </div>
         <div className="title">{title}</div>
         <p className="description" dangerouslySetInnerHTML={{ __html: excerpt }} />
         <div className="info">
           <div className="date">{date}</div>
-          <div className="categories">
-            {categories.map((category) => (
-              <div className="category" key={category} to={`/posts/${category}`}>
-                {category}
-              </div>
-            ))}
-          </div>
         </div>
       </Link>
     </div>
